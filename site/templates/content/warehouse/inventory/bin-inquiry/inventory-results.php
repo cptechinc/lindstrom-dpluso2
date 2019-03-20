208684a1569e557ea1825a082f1dd47471187525
Paul Gomez <pauldro952@outlook.com>
2019-03-20 15:21:52 -0500

    fixes on inventory resultss
    Includes Libarary dplus-warehouse updates
    
    (cherry-picked from cptechinc/soft-dpluso@e76cee8367e283334890a1b6029203ccf1fef1b4)

---
+++ site/templates/content/warehouse/inventory/bin-inquiry/inventory-results.php
@@ -0,0 +1,8 @@
+<div>
+	<div class="form-group">
+		<a href="<?= $page->url; ?>" class="btn btn-primary not-round">
+			<i class="fa fa-arrow-left" aria-hidden="true"></i> Return to Bin Inquiry
+		</a>
+	</div>
+	<?php include __DIR__."/inventory-results-list.php"; ?>
+</div>
